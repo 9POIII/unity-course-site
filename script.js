f6df30fac918d2b652cabc07d6b12d08a366b2f9
Daniel <9POIII@users.noreply.github.com>
2025-09-16 02:58:16 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Акордеон
+const buttons = document.querySelectorAll(".accordion-btn");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("active");
+    const content = btn.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+});
