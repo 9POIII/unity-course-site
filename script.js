9c46344b7fc9357f0d3bc95495b1c6e9461a9857
Daniel <9POIII@users.noreply.github.com>
2025-09-17 01:22:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ buttons.forEach(btn => {
     }
   });
 });
+
+document.getElementById("contactForm").addEventListener("submit", async function(e) {
+  e.preventDefault();
+
+  const phone = this.phone.value;
+  const telegram = this.telegram.value;
+
+  try {
+    const res = await fetch("https://script.google.com/macros/s/AKfycbyFUMEOyU_2jwqourqmXyRea0xyAFM4vrfqrruiUJp_OLxyxhivWIosEqEgORI1rb1ZSg/exec", {
+      method: "POST",
+      body: JSON.stringify({ phone, telegram })
+    });
+
+    const result = await res.json();
+    document.getElementById("status").innerText = result.ok 
+      ? "✅ Заявку надіслано!" 
+      : "❌ Сталася помилка. Спробуйте ще раз.";
+  } catch (err) {
+    document.getElementById("status").innerText = "⚠️ Немає з'єднання з сервером.";
+  }
+});
